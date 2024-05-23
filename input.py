54d42d86949928b6b9461820286fa83a8ed12b35
Diego <100723562+jtoledom1@users.noreply.github.com>
2024-05-23 02:38:38 -0600

    Create input.py (#2)
    * Create input.py
    
    * LLL

---
+++ input.py
@@ -0,0 +1,13 @@
+#esto es un comentario 
+# Todo producto de la función input es una cadena
+
+print("Ingresa la información...")
+anything = input()
+print("Tu información fué: ", anything, "¿verdad?")
+
+# Para hacer operaciones con lo ingresado por el usuario se debe de modificar
+#con la finalidad de ser entendido como un número P/E
+
+an = float(input("Inserta un número: "))
+Smt= an ** 2.0
+print(anything, "al cuadrado es", Smt)
