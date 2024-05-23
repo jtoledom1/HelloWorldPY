53322f27e69832f23414e6652014429a9cd92c54
Diego <100723562+jtoledom1@users.noreply.github.com>
2024-05-23 02:44:05 -0600

    Update input.py

--- input.py
+++ input.py
@@ -11,3 +11,10 @@ print("Tu información fué: ", anything, "¿verdad?")
 an = float(input("Inserta un número: "))
 Smt= an ** 2.0
 print(anything, "al cuadrado es", Smt)
+
+# La función type nos arroja el tipo de dato que se encuentra dentro de nuestra variable
+
+x = input("Ingresa un número: ") # El usuario ingresa un 2 
+# A pesar de haber introducido un número la respuesta será que es un string porque no se transformó a número
+# Toda entrada de input sin transformar, es directamente un string
+print(type(x))
