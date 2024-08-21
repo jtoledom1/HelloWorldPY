5ecf6a6d15e599b29defa6bf4f6bf8e252ae1352
jtoledom1 <jtoledom1@outlook.com>
2024-08-21 13:45:57 -0600

    🦄Documentación

--- Suam2num.py
+++ Suam2num.py
@@ -2,3 +2,4 @@ num1 = int(input("Dame un núm "))
 num2 = int(input("Dame un núm "))
 suma = num1 + num2
 print(suma)
+#De no haber puesto la instrucción de int, el resultado habría sido una concatenación y no una suma
