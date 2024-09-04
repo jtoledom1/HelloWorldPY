2c21bc294a42037e7be9c28f04cdf07c0775072c
jtoledom1 <jtoledom1@outlook.com>
2024-09-04 14:03:23 -0600

    Com instlibre 🦖

--- ClaseJordan/Clase1.py
+++ ClaseJordan/Clase1.py
@@ -1,2 +1,9 @@
 from math import sin
 print (sin(30))
+
+from math import sin, radians
+
+print(sin(radians(30)))
+
+#Para instalar las librerías symphony numpy matplotlib el comando es:
+#pip3 install symphony numpy matplotlib
