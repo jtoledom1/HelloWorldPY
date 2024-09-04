8c56d19ec6454cf79330e91110a37e68b8a75f91
jtoledom1 <jtoledom1@outlook.com>
2024-09-04 14:09:28 -0600

    Concatenar 🦖

--- ClaseJordan/Clase1.py
+++ ClaseJordan/Clase1.py
@@ -7,3 +7,6 @@ print(sin(radians(30)))
 
 #Para instalar las librerías symphony numpy matplotlib el comando es:
 #pip3 install symphony numpy matplotlib
+
+#Para hacer concatenaciones se hace lo sig:
+print(2," ", "Esto es una concatenación")
