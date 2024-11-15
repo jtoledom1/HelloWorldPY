87d14b68f2d2614c81fc23a41e829ac68f03c7cb
jtoledom1 <jtoledom1@outlook.com>
2024-11-15 14:33:22 -0600

    🦖 trivia colorfull

--- ClaseJordan/Proys/ordinario/ordinario.py
+++ ClaseJordan/Proys/ordinario/ordinario.py
@@ -1,47 +1,81 @@
 import time
 from os import system
+
+RED = '\033[0;31m'
+CYAN = '\033[0;36m'
+GREEN = '\033[0;32m'
+BLUE = '\033[0;34m'
+def ray():
+    print(f"{CYAN}----------------------------------------")
+
+def clar():
+        system("clear")
+
 def entrada ():
-    CYAN = '\033[0;36m'
     print("")
-    system("clear")
+    clar()
     print(f"""
     {CYAN}  
-    :::::::::::       ::::::::       :::        ::::::::::       :::::::::       :::::::: 
-        :+:          :+:    :+:      :+:        :+:              :+:    :+:     :+:    :+: 
-        +:+          +:+    +:+      +:+        +:+              +:+    +:+     +:+    +:+  
-        +#+          +#+    +:+      +#+        +#++:++#         +#+    +:+     +#+    +:+   
-        +#+          +#+    +#+      +#+        +#+              +#+    +#+     +#+    +#+    
-        #+#          #+#    #+#      #+#        #+#              #+#    #+#     #+#    #+#     
-        ###           ########       ########## ##########       #########       ########       
+        :::::::::::       ::::::::       :::            ::::::::::       :::::::::       :::::::: 
+            :+:          :+:    :+:      :+:            :+:              :+:    :+:     :+:    :+: 
+            +:+          +:+    +:+      +:+            +:+              +:+    +:+     +:+    +:+  
+            +#+          +#+    +:+      +#+            +#++:++#         +#+    +:+     +#+    +:+   
+            +#+          +#+    +#+      +#+            +#+              +#+    +#+     +#+    +#+    
+            #+#          #+#    #+#      #+#            #+#              #+#    #+#     #+#    #+#     
+            ###           ########       ##########     ##########       #########       ########       
 
         """)
     
-entrada()
-
-def mostrar_contador():
+    print("Bienvenido a esta trivia, espero te diviertas y pongas a prueba tus conocimientos, esta es una breve\npero interesante serie de preguntas, tus errores se mostrarán en rojo y es muy importante que uses\nun sist operativo tipo unix, ya que de lo contrario no funcionará el programa como es debido.")
+    time.sleep(3)
+def mostrar_contador(frase_opner):
+    time.sleep(1)
+    clar()
+    ray()
+    print(f"{GREEN}\n" + frase_opner + "\n")
     for i in range(3, 0, -1):
         print(i)
         time.sleep(1)
+    ray()
+    time.sleep(1)
+    clar()
 
 def hacer_pregunta(pregunta, opciones, respuesta_correcta, puntaje):
-    print("\n" + pregunta)
+    ray()
+    print(f"{BLUE}\n" + pregunta)
+    # Este bucle for recorre la lista opciones, y para cada elemento, 
+    # idx tendrá el número de índice (empezando desde 1) 
+    # y opcion tendrá el valor de ese elemento.
+    # de tal forma que idx es el numerito en las opciones de salida :)
     for idx, opcion in enumerate(opciones, 1):
-        print(f"{idx}. {opcion}")
+        print(f"{CYAN}{idx}. {opcion}{BLUE}")
     
-    respuesta = input("Respuesta: ")
-    if respuesta.isdigit() and int(respuesta) == respuesta_correcta:
+    while True:
+        respuesta = input(f"{BLUE}Respuesta:{RED} ")
+        
+        # Validamos que la respuesta sea un número entero entre 1 y 3
+        if respuesta.isdigit():
+            respuesta_numero = int(respuesta)
+            if 1 <= respuesta_numero <= 3:
+                break
+            else:
+                print("Por favor, elige un número entre 1 y 3.")
+        else:
+            print("Por favor, ingresa un número válido entre 1 y 3.")
+    
+    # Comprobamos si la respuesta es correcta
+    if respuesta_numero == respuesta_correcta:
         puntaje += 10
-        print(f"Puntaje agregado =  10\nTotal hasta ahora =  {puntaje}")
+        print(f"{BLUE}Puntaje agregado =  {RED}10\n{BLUE}Total hasta ahora = {RED}{puntaje}{BLUE}")
     else:
-        print(f"Puntaje agregado =  0\nTotal hasta ahora =  {puntaje}")
-    
+        print(f"{BLUE}Puntaje agregado =  {RED}0\n{BLUE}Total hasta ahora = {RED}{puntaje}{BLUE}")
+    ray()
     return puntaje
-
 def main():
     puntaje = 0
     
     # Pregunta 1
-    mostrar_contador()
+    mostrar_contador("Espero hayas estudiado Geografía...")
     puntaje = hacer_pregunta(
         "¿Cuál es la capital de Rusia?", 
         ["Moscú", "Yugoslavia", "Medellín"], 
@@ -50,16 +84,16 @@ def main():
     )
 
     # Pregunta 2
-    mostrar_contador()
+    mostrar_contador("Ahora pasemos de la Tierra al Espacio, ¿Quieres?")
     puntaje = hacer_pregunta(
-        "¿Qué es considerado Putón?", 
+        "¿Qué es considerado Plutón?", 
         ["Planeta", "Planeta enano", "Satélite de Neptuno"], 
         2, 
         puntaje
     )
 
     # Pregunta 3
-    mostrar_contador()
+    mostrar_contador("¡¡Bienvenido a Juegos Mentales!!")
     puntaje = hacer_pregunta(
         "¿Si 3=1, cuanto es 1*(6/2)+2?", 
         ["5", "1", "3"], 
@@ -68,7 +102,7 @@ def main():
     )
 
     # Pregunta 4
-    mostrar_contador()
+    mostrar_contador("Hablemos de algo importante...")
     puntaje = hacer_pregunta(
         "En porcentaje, ¿Cuánta agua hay en el Planeta Tierra?", 
         ["2.5%", "70%", "60%"], 
@@ -77,7 +111,7 @@ def main():
     )
 
     # Pregunta 5
-    mostrar_contador()
+    mostrar_contador("Demos un viaje al pasado")
     puntaje = hacer_pregunta(
         "¿Quiénes hicieron las pirámides de Egipto?", 
         ["Esclavos", "Aliens", "Trabajadores y esclavos"], 
@@ -87,7 +121,7 @@ def main():
 
     # Resultado final
     print("\n--------------------")
-    mostrar_contador()
+    mostrar_contador("Eso es to, eso es to, eso es todo amigos!!")
     print(f"Puntaje final =  {puntaje}")
     if puntaje < 30:
         print("Deberías estudiar un poco más")
@@ -97,5 +131,6 @@ def main():
     print("¡Gracias por jugar!")
 
 if __name__ == "__main__":
+    entrada()
     main()
 
