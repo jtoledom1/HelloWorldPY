2968b90d3acb3dc20178eaab4aec87f35f09c536
jtoledom1 <jtoledom1@outlook.com>
2024-11-15 11:51:04 -0600

    ⚡️ reorder

--- ClaseJordan/prettyprint.py
+++
@@ -1,8 +0,0 @@
-from sympy import *
-from sympy import init_printing
-from sympy.abc import x
-
-init_printing() 
-funcion = sqrt(1/x)
-mostrar_integral = Integral(funcion,x)
-pprint(mostrar_integral)
