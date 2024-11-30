22a22afb14243745172227254c05e84663f20309
jtoledom1 <jtoledom1@outlook.com>
2024-11-30 12:59:57 -0600

    ⚡️ py reorder

---
+++ Ordinario DB/API/actions.py
@@ -0,0 +1,66 @@
+from func import interacmongo
+def menu(Resp):
+    match Resp:
+        case '1':
+            interacmongo('insert_many',query=[
+                {
+                    "codigo": "016",
+                    "nombre": "Sheldon",
+                    "AP": "Cooper",
+                    "AM": "Martínez",
+                    "edad": 34,
+                    "Telefono": ["+521234567890", "+529876543210"],
+                    "Temas": ["viajes", "gastronomía", "futbol"]
+                },
+                {
+                    "codigo": "017",
+                    "nombre": "Missy",
+                    "AP": "Sánchez",
+                    "AM": "Ramírez",
+                    "edad": 28,
+                    "Telefono": ["+521111223344", "+529876512345"],
+                    "Temas": ["moda", "literatura", "películas", "tecnología"]
+                }
+        ])
+        case '2':
+            interacmongo('insert_one',query=[
+                {
+                    "codigo": "015",
+                    "nombre": "Sheldon",
+                    "AP": "Cooper",
+                    "AM": "Martínez",
+                    "edad": 34,
+                    "Telefono": ["+521234567890", "+529876543210"],
+                    "Temas": ["viajes", "gastronomía", "futbol"]
+                }
+        ])
+        case '3':
+            interacmongo('insert_many',query=[
+                {
+                    "ident":"001",
+                    "colores":["Azul","Rojo","Amarillo"],
+                    "precio":23000,
+                    "Existencia":12,
+                    "nombre":"Cofre",
+                    "Materiales":["Metal","Madera"]
+
+                },
+                {
+                    "ident":"002",
+                    "colores":["Azul","Rojo","Amarillo"],
+                    "precio":12500,
+                    "Existencia":12,
+                    "nombre":"Pala",
+                    "Materiales":["Hierro","Madera"]
+
+                },
+                {
+                    "ident":"003",
+                    "colores":["Reflejante","Polarizado","Transparente"],
+                    "precio":1000,
+                    "Existencia":12,
+                    "nombre":"Ventana",
+                    "Materiales":["Aluminio","Cristal"]
+                }
+                ])            
+menu(input("\n\nOpción\n\n\n"))
---
+++ Ordinario DB/API/conect.py
@@ -0,0 +1,32 @@
+from pymongo import MongoClient
+def interacmongo(TolAct,query={}):
+    try:
+        client = MongoClient('localhost', 27017)
+        database = client['admin']
+        collection = database['toleletee']
+        documents = collection.find()
+        
+        for document in documents:
+            print(document)
+
+        match TolAct:
+            case 'find':
+                collection.find(query)
+            case 'remove':
+                collection.remove(query, True)
+            case 'updateOne':
+                collection.updateOne(query)
+            case 'insert_many':
+                collection.insert_many(query)
+            case 'insertOne':
+                collection.insert_one(query)
+
+            case _:
+                print("Acción no reconocida")
+
+    except Exception as ex:
+        print("Error durante la conexión: {}".format(ex))
+    finally:
+        client.close()
+        print("Éxito.")
+
